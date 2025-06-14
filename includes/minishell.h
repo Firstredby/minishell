f2f0e9771fb064b7bed82d9804c2544d7462b2de
Firstredby <imshchirov@gmail.com>
2025-06-14 22:41:58 +0200

    structurized

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ishchyro <ishchyro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 10:25:57 by aorth             #+#    #+#             */
-/*   Updated: 2025/06/14 12:08:29 by codespace        ###   ########.fr       */
+/*   Updated: 2025/06/14 21:23:29 by ishchyro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,31 +47,35 @@ void    cmd_cleaner(t_cmd *cmd);
 void    env_cleaner(t_env *env);
 void    free_all(t_cmd *cmd, t_env *env, t_token **token);
 
-int ft_echo(t_cmd *cmd);
-int ft_pwd(t_cmd *cmd);
-int ft_cd(t_cmd *cmd);
-int ft_exit(t_cmd *cmd);
-void    exe_cmd(t_cmd *cmd, t_env *env);
-void    redir_append(t_cmd *cmd, int i);
-void    redir_out(t_cmd *cmd, int i);
-void    redir_in(t_cmd *cmd, int i);
-void    handle_redir(t_cmd *cmd);
-int     is_builtin(t_cmd *cmd);
+//error messages
+void	syn_err(t_token *token);
+void	miss_quote(void);
+void	undef_cmd(char *str);
+
+//builtins
+int		ft_echo(t_cmd *cmd);
+int		ft_pwd(t_cmd *cmd);
+int		ft_cd(t_cmd *cmd);
+int		ft_exit(t_cmd *cmd);
+int		is_builtin(t_cmd *cmd);
+int		ft_env(t_cmd *cmd, t_env *env);
+int		ft_export(t_cmd *cmd, t_env *env);
+int		ft_unset(t_cmd *cmd, t_env *env);
 void    run_builtin(t_cmd *cmd, t_env *env);
-void    ft_free_split(char **split);
-void    execute_pipe(t_cmd *cmd1, t_cmd *cmd2);
-int ft_env(t_cmd *cmd, t_env *env);
-int ft_export(t_cmd *cmd, t_env *env);
-int    ft_unset(t_cmd *cmd, t_env *env);
-void execute_pipe2(t_cmd *cmd, t_env *env);
-void	ll_free(t_cmd *cmd);
+
+//env
+int		env_add(t_env **env_head, char *env);
+t_env	*env_create(char *env);
+
+//execution
+void	exe_cmd(t_cmd *cmd, t_env *env);
+void	execute_pipe(t_cmd *cmd, t_env *env);
 void    handle_redirV2(t_cmd *cmd);
+
+//execution preparation
 void    handle_heredoc(t_cmd *cmd);
-void    handle_heredocv2(t_cmd *cmd);
 char    *create_filename(char *path, char *id, char *type);
 void    exe_prep(t_cmd *cmd);
-int env_add(t_env **env_head, char *env);
-t_env *env_create(char *env);
 
 //utils
 void	*ft_calloc(size_t nmemb, size_t size);
@@ -89,7 +93,7 @@ char	**ft_split(char const *s, char c);
 int		env_handle(char **env, t_env **env_head);
 int		env_strcmp(const char *s1, const char *s2);
 size_t	command_count(char *input);
-void	ft_strjoin_free(char **s1, char const *s2);
+void	ft_strjoin_free(char **s1, char *s2);
 char	*ft_itoa(int n);
 
 //debug panel
