e9f8b693a78a07d76531f0a4b6da55fa03261a9d
Firstredby <imshchirov@gmail.com>
2025-06-19 22:03:30 +0200

    added extra check for folders

--- src/utils/error_messages.c
+++ src/utils/error_messages.c
@@ -6,7 +6,7 @@
 /*   By: ishchyro <ishchyro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 17:14:44 by ishchyro          #+#    #+#             */
-/*   Updated: 2025/06/17 19:47:32 by ishchyro         ###   ########.fr       */
+/*   Updated: 2025/06/19 21:56:39 by ishchyro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	syn_err(t_token *token)
 {
-	g_exit_status = 2;
 	ft_putstr_fd("syntax error near unexpected token ", 2);
 	ft_putstr_fd("`", 2);
 	if (token)
@@ -22,25 +21,47 @@ void	syn_err(t_token *token)
 	else
 		ft_putstr_fd("newline", 2);
 	ft_putstr_fd("'\n", 2);
+	g_exit_status = 2;
 }
 
 void	miss_quote(void)
 {
-	g_exit_status = 2;
 	ft_putstr_fd("minishell: no closing quote\n", 2);
+	g_exit_status = 2;
+}
+
+int	directory(char *str)
+{
+	DIR *dir;
+	
+	dir = opendir(str);
+	if (!dir)
+		return (1);
+	closedir(dir);
+	return (0);
 }
 
 void	undef_cmd(char *str)
 {
-	g_exit_status = 127;
-	if (str)
+	
+	if (str && ((*str == '.' && *(str + 1) == '/') || *str == '/'))
+	{
+		if (directory(str))	
+			return (file_not_exists(str), (void)(g_exit_status = 127));
+		else
+			return (ft_putstr_fd(str, 2),
+				ft_putstr_fd(": Is a directory\n", 2),
+				(void)(g_exit_status = 126));
+	}
+	if (str && *str)
 		ft_putstr_fd(str, 2);
 	ft_putstr_fd(": command not found\n", 2);
+	g_exit_status = 127;
 }
 
 void	file_not_exists(char *str)
 {
-	g_exit_status = 1;
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd(": No such file or directory\n", 2);
+	g_exit_status = 1;
 }
