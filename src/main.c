b85b806286d1f44dba3d43cd684d02b029a2384d
Adrian Orth <aorth@c2r7p5.42vienna.com>
2025-06-29 22:26:12 +0200

    main norm edits

--- src/main.c
+++ src/main.c
@@ -19,12 +19,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int	g_exit_status = 0;
 
 int	cmd_init(t_data *data, char *input, char **envp)
 {
 	if (!data->env)
+	{
 		if (!env_handle(envp, &data->env))
 		{
 			if (data->env)
@@ -32,9 +34,10 @@ int	cmd_init(t_data *data, char *input, char **envp)
 			else
 			{
 				data->env = NULL;
-				return(12);
+				return (12);
 			}
 		}
+	}
 	data->token = tokenizer(input, command_count(input));
 	if (!data->token && g_exit_status == 12)
 		return (free_all(NULL, data->env, NULL), 12);
@@ -53,15 +56,54 @@ int	start_exec(t_data *data)
 {
 	if (exe_prep(data->cmd))
 	{
-		free_all(data->cmd, data->env, data->token);
-		return (g_exit_status = 12);
+		// if (g_exit_status == 12) {
+		// 	free_all(data->cmd, data->env, data->token);
+		// 	return (g_exit_status = 12);
+		// }
+		// else 
+		return(1);
 	}
 	command_sigs();
 	if (!data->cmd->next)
+	{
 		exe_cmd(data->cmd, &data->env, data);
+	}
 	else
 		execute_pipe(data->cmd, data->env, data);
 	return (0);
+}
+
+// int	main_helper(char *input)
+// {
+// 	main_sigs();
+// 	input = readline("minishell$ ");
+// 	if (input)
+// 		add_history(input);
+// 	if (!input)
+// 	{
+// 		printf("exit\n");
+// 		return(0);
+// 	}
+// 	if (input[0] == '\0')
+// 	{
+// 		free(input);
+// 		return(1);
+// 	}
+// }
+
+static int	main_cleaner(t_data *data, int should_exit)
+{
+		cmd_cleaner(data->cmd);
+		data->cmd = NULL;
+		if (data->token)
+		{
+			trash_collector_goes_brrrr(data->token);
+			data->token = NULL;
+		}
+		if (should_exit)
+			return(0);
+		else
+		 	return(1);
 }
 
 int	main_loop(t_data *data, char **envp)
@@ -69,9 +111,11 @@ int	main_loop(t_data *data, char **envp)
 	int		check;
 	int		should_exit;
 	char	*input;
+	int		test;
 
 	input = NULL;
 	should_exit = 0;
+	test = 0;
 	while (1)
 	{
 		main_sigs();
@@ -88,6 +132,9 @@ int	main_loop(t_data *data, char **envp)
 			free(input);
 			continue ;
 		}
+		// test = main_helper(input);
+		// if (!test)
+		// 	break;
 		check = cmd_init(data, input, envp);
 		if (input)
 		{
@@ -103,24 +150,29 @@ int	main_loop(t_data *data, char **envp)
 			should_exit = 1;
 		envp = NULL;
 		if (start_exec(data))
-			return (free_all(data->cmd, data->env, data->token), free(data),
+			return (free_all(data->cmd, NULL, data->token),
 				g_exit_status);
-		cmd_cleaner(data->cmd);
-		data->cmd = NULL;
-		if (data->token)
-		{
-			trash_collector_goes_brrrr(data->token);
-			data->token = NULL;
-		}
-		if (should_exit)
-			break ;
+
+		//show_args(data->cmd);
+		//close(data->cmd->fd_in);
+		if(!main_cleaner(data, should_exit))
+			break;
+		// cmd_cleaner(data->cmd);
+		// data->cmd = NULL;
+		// if (data->token)
+		// {
+		// 	trash_collector_goes_brrrr(data->token);
+		// 	data->token = NULL;
+		// }
+		// if (should_exit)
+		// 	break ;
 	}
 	return (0);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_data *data;
+	t_data	*data;
 
 	((void)argc, (void)argv);
 	data = ft_calloc(1, sizeof(t_data));
